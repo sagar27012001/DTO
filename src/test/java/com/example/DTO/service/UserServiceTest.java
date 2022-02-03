797a5c2e40f69b8b8d4f70f47912fb10b0f162b2
sagar27012001 <sagarpatil27012001@gmail.com>
2022-02-03 17:22:46 +0530

    JUnit added

--- src/test/java/com/example/DTO/service/UserServiceTest.java
+++ src/test/java/com/example/DTO/service/UserServiceTest.java
@@ -3,6 +3,7 @@ package com.example.DTO.service;
 import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
@@ -10,9 +11,11 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 @WebMvcTest(UserService.class)
 public class UserServiceTest {
 
+    @Autowired
+    private UserService userService;
+
     @Test
     public void testGetUserLocation() {
-        UserService userService = new UserService();
         assertEquals("User not found", userService.getUserLocation("1").getBody().toString());
     }
 }
